3bb57ca7bf679efd7721e84fc09d8533424ee6f2
ibktommy <atomquartz@gmail.com>
2022-07-27 00:07:05 +0100

    Create a Separate Util Folder that will hold our functions so as to minimize code on the AppJS and also make our folder structure much elegant

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Card from './component/Card/Card';
 import Form from './component/Form/Form';
+import { getData } from './utils/fetch';
 
 function App() {
   const [username, setUsername] = useState("");
@@ -9,31 +10,8 @@ function App() {
   const [imageUrl, setImageUrl] = useState(null);
   const [data, setData] = useState([])
 
-  const getData = () => {
-    fetch('data.json', {
-      headers: {
-        'Content-Type': 'appplication/json',
-        'Accept': 'appplication/json'
-      }
-    })
-      .then((response) => {
-        return response.json()
-      })
-      .then((responseData) => {
-        let userData = responseData
-
-        // Setting Data-State based on the condition - if there exist a data in localStorage or Not
-        let getLocalStorage = localStorage.getItem('data')
-        if (getLocalStorage) {
-          setData(JSON.parse(getLocalStorage))
-        } else {
-          setData(userData)
-        }
-      })
-  }
-
   useEffect(() => {
-    getData()
+    getData(setData)
   }, [])
 
   const handleName = (e) => {
