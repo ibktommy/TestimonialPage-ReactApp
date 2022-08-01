f4c08a07a740cbf800fae83afc66bdaab307c794
ibktommy <atomquartz@gmail.com>
2022-08-01 22:33:23 +0100

    Refactoring the AppJS file

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import Card from './component/Card/Card';
-import Form from './component/Form/Form';
-import { getData } from './utils/fetchData';
+import React, { useEffect, useState } from 'react'
+import UserCard from './component/Card/UserCard'
+import Form from './component/Form/Form'
+import { fetchData } from './utils/fetchData'
 
-function App() {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const [imageFile, setImageFile] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
+const App = () => {
+  // Setting State
   const [data, setData] = useState([])
 
-  // Getting the Data and setting it in data-state using useEffect after the component renders
+  // Setting useEffect Hook to get Data after Component Renders
   useEffect(() => {
-    getData(setData)
+    fetchData(setData)
   }, [])
 
   console.log(data)
 
-  // Delete a Chosen Card
-  const deleteTestimonyCard = (id) => {
-    setData(data.filter((eachData) => eachData.id !== id))
+  //Function To Get Data From Form-Component
+  function addUserDataHandler(newUserData) {
+    setData([...data, newUserData])
+
+    // Save Data to Local Storage
+    localStorage.setItem('data', JSON.stringify([...data, newUserData]))
   }
 
-  console.log(data)
-
   return (
-    <>
-      <div className="container flex">
-        <h1>Testimonial Page</h1>
-
-        <div className="container-content flex">
-          {
-            data && data.length > 0 && data.map(({ id, ...props }) => (
-              <Card key={id} {...props} deleteTestimonyCard={deleteTestimonyCard} data={data} />
-            ))
-          }
-        </div>
-
-        <Form
-          username={username}
-          message={message}
-          imageFile={imageFile}
-          imageUrl={imageUrl}
-          setMessage={setMessage}
-          setUsername={setUsername}
-          setImageFile={setImageFile}
-          setImageUrl={setImageUrl}
-          data={data}
-          setData={setData}
-        />
-      </div>
-    </>
-  );
+    <div>
+      <UserCard usersData={data} />
+      <Form onAddUserData={addUserDataHandler} dataNum={data} />
+    </div>
+  )
 }
 
-export default App;
+export default App
